b7a3598bb7abd2bb0168ea1eb6797ca7577be109
Derek Gransaull <derek@dgtlife.com>
2016-11-11 20:00:09 -0800

    ES6ified and improved to v0.8.5

--- nav-utils.js
+++ nav-utils.js
@@ -5,143 +5,99 @@
  *
  * Created on 12/13/14
  */
+/* eslint new-cap: ["error", { "capIsNewExceptionPattern": "^Match\.." }] */
+import { check, Match } from 'meteor/check';
 
 /**
  * @namespace Utils
- * @summary The namespace for all Nav utility functions and variables
+ * @summary The namespace for all Navigate utility functions and variables
  */
-
-/*jshint -W117 */    // app- and package-scoped variables not detected by JSHint
-/*jshint -W058 */    // no () for constructor
-
-Utils = {};
-
-// Match pattern for a non-empty string
-Utils.nonEmptyString = Match.Where(
-  function (value) {
-    "use strict";
-
+const Utils = {
+  // Match pattern for a non-empty string
+  nonEmptyString: Match.Where((value) => {
     check(value, String);
     return value.length > 0;
-  }
-);
+  }),
 
-// Match pattern for function.
-Utils.function = Match.Where(
-  function (val) {
-    "use strict";
+  // Match pattern for function.
+  function: Match.Where(val => val instanceof Function),
 
-    return (val instanceof Function);
-  }
-);
+  // Match pattern for RegExp.
+  regExp: Match.Where(val => val instanceof RegExp),
 
-// Match pattern for RegExp.
-Utils.regExp = Match.Where(
-  function (val) {
-    "use strict";
+  // Match pattern for string or function.
+  nonEmptyStringOrFunction: Match.Where(
+    val => Match.test(val, Utils.nonEmptyString) || (val instanceof Function)
+  ),
 
-    return (val instanceof RegExp);
-  }
-);
+  // Match pattern for null or function.
+  nullOrFunction: Match.Where(
+    val => Match.test(val, null) || (val instanceof Function)
+  ),
 
-// Match pattern for string or function.
-Utils.nonEmptyStringOrFunction = Match.Where(
-  function (val) {
-    "use strict";
+  // Match pattern for undefined or function.
+  undefinedOrFunction: Match.Where(
+    val => Match.test(val, undefined) || (val instanceof Function)
+  ),
 
-    return Match.test(val, Utils.nonEmptyString) || (val instanceof Function);
-  }
-);
+  // Match pattern for boolean or function.
+  booleanOrFunction: Match.Where(
+    val => (typeof val === 'boolean') || (val instanceof Function)
+  ),
 
-// Match pattern for null or function.
-Utils.nullOrFunction = Match.Where(
-  function (val) {
-    "use strict";
+  /**
+   * Update the browser history to enable support for Back and Forward buttons
+   * used in browser mode.
+   * @param {string} name - the name of the current screen
+   * @param {string} path - the path (unparameterized case) associated with the
+   *                        screen
+   * @param {string} pathMask - the mask for a parameterized path
+   * @param {function} generatePath - the definition for the function that
+   *                                  generates the path from the path mask and
+   *                                  the screen data (it's screen-specific)
+   * @param {object} screenData - the screen data associated with a
+   *                              parameterized path
+   * @private
+   */
+  updateBrowserHistory(name, path, pathMask, generatePath, screenData) {
+    // Initialize the screen state object.
+    const screenState = {
+      sid: (function getSid() {
+        if (_.isNull(window.history.state)) {
+          return 1;
+        }
 
-    return Match.test(val, null) || (val instanceof Function);
-  }
-);
+        return 1 + window.history.state.sid;
+      }()),
+      name: name
+    };
 
-// Match pattern for undefined or function.
-Utils.undefinedOrFunction = Match.Where(
-  function (val) {
-    "use strict";
-
-    return Match.test(val, undefined) || (val instanceof Function);
-  }
-);
-
-// Match pattern for boolean or function.
-Utils.booleanOrFunction = Match.Where(
-  function (val) {
-    "use strict";
-
-    return (typeof val === 'boolean') || (val instanceof Function);
-  }
-);
-
-
-// Wait for condition (to be true) before running a function.
-Utils.waitForCondition = function (condition, func, showLoading, context) {
-  "use strict";
-
-  var conditionSatisfied;
-
-  context = context || null;
-
-  // Check for proper inputs.
-  check(condition, Utils.nonEmptyStringOrFunction);
-  check(func, Utils.function);
-  check(showLoading, Boolean);
-
-  // Context may not be a plain object.
-  if (!_.isNull(context)) {
-    check(context, Match.Where(
-      function (val) {
-        return _.isObject(val);
-      }
-    ));
-  }
-
-  if (condition instanceof Function) {
-    conditionSatisfied = condition;
-  } else {
-
-    // It is a conditionId.
-    if (_.has(Nav._config.conditionsToWaitFor, condition)) {
-      conditionSatisfied = function () {
-        Nav._config.conditionsToWaitFor[condition].forEach(function (conditionFunc) {
-          if (conditionFunc() === false) {
-            return false;
-          }
-        });
-        return true;
-      };
-
+    // Add screen data if it is provided.
+    let params;
+    if (screenData) {
+      _.extend(screenState, { screenData: screenData });
+      params = screenData.params;
     } else {
-      // Throw an error.
-      error.message = 'An unknown condition was specified.';
-      throw error;
+      params = null;
+    }
+
+    if (pathMask && params && !path) {
+      /*
+       * This is a parameterized path. Construct the pathname by using the
+       * path mask and the parameters provided.
+       */
+      const _path = generatePath(pathMask, params);
+
+      // Push the state, document.title, and path into browser history.
+      window.history.pushState(screenState, document.title, _path);
+    } else {
+      /*
+       * This is a literal path. Push the state, document.title, and path into
+       * the browser history.
+       */
+      window.history.pushState(screenState, document.title, path);
     }
   }
-
-  Tracker.autorun(function (comp) {
-    if (! conditionSatisfied()) {
-
-      // Show the 'Loading' screen, and set the 'Loading' state.
-      Nav.session.set('Loading', true);
-      Nav.session.set(Nav._config.contentHelpers, 'loading');
-    } else {
-      Nav.session.set('Loading', false);
-
-      // Call the function that requires the condition to be true, passing the
-      // data context to it, if necessary.
-      if (_.isNull(context)) {
-        func();
-      } else {
-        func(context);
-      }
-      comp.stop();
-    }
-  });
 };
+
+export default Utils;
--- package.js
+++ package.js
@@ -1,25 +1,26 @@
 /**
- * @file Define the App Navigator package
+ * @file Define the dgtlife:navigate package
  * @author Derek Gransaull <derek@dgtlife.com>
  * @copyright DGTLife, LLC 2014
  *
  * Created on 1/15/14
  */
 Package.describe({
-  summary: "App Navigator is a simple lightweight navigator package for Meteor-based web apps",
-  version: '0.8.3'
+  summary: 'Navigate is a simple lightweight navigation package for ' +
+           'Meteor-based web apps',
+  version: '0.8.5',
+  name: 'dgtlife:navigate',
+  git: ''
 });
 
-Package.onUse(function (api) {
+Package.onUse((api) => {
+  api.use('ecmascript', 'client');
   api.use('templating', 'client');
   api.use('spacebars', 'client');
   api.use('reactive-dict', 'client');
   api.use('jquery', 'client');
-  api.use('underscore', ['client', 'server']);
-  api.use('check', ['client', 'server']);
-
-  api.addFiles('nav-utils.js', ['client', 'server']);
-  api.addFiles('navigator.js', ['client', 'server']);
-
-  api.export('Nav', ['client', 'server']);
+  api.use('underscore', 'client');
+  api.use('check', 'client');
+  api.mainModule('navigate.js', 'client');
+  api.addFiles('nav-utils.js', 'client');
 });
